be7896cd72e45652bd5510c909b2e9c97178d2e1
washingtonab <washington.ab@gmail.com>
2019-11-20 23:16:35 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <h1>{{ title }}</h1>
+
+    <form
+      @submit="checkForm"
+      action="https://vuejs.org/"
+      method="post"
+    >
+
+      <p v-if="errors.length">
+        <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Nome</label>
+        <input
+          id="name"
+          v-model="name"
+          type="text"
+          name="name"
+        >
+      </p>
+
+      <p>
+        <label for="age">Idade</label>
+        <input
+          id="age"
+          v-model="age"
+          type="number"
+          name="age"
+          min="0"
+        >
+      </p>
+
+      <p>
+        <label for="movie">Filme favorito</label>
+        <select
+          id="movie"
+          v-model="movie"
+          name="movie"
+        >
+          <option>Star Wars</option>
+          <option>Vanilla Sky</option>
+          <option>Atomic Blonde</option>
+        </select>
+      </p>
+
+      <p>
+        <input
+          type="submit"
+          value="Enviar"
+        >
+      </p>
+
+    </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "Welcome to Thunderstruck",
+      errors: [],
+      name: null,
+      age: null,
+      movie: null
+    }
+  },
+  methods:{
+    checkForm: function (e) {
+      if (this.name && this.age) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push('O nome é obrigatório.');
+      }
+      if (!this.age) {
+        this.errors.push('A idade é obrigatória.');
+      }
+
+      e.preventDefault();
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
